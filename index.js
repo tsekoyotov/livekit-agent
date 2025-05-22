bbb79fec119cf4dff2b49c9135ea60934c4d0cc1
tsekoyotov <musclefreakbg@gmail.com>
2025-05-22 20:09:07 +0300

    fix: correct LiveKit SDK package name

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const { AccessToken } = require('@livekit/server-sdk');
+const { AccessToken } = require("livekit-server-sdk");
 
 const app = express();
 app.use(express.json());
