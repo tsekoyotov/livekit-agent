5eefaaf7994d56f192aae0bddb5dd16aa52c20ca
tsekoyotov <musclefreakbg@gmail.com>
2025-05-22 20:30:28 +0300

    chore: listen on process.env.PORT for Railway

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.post('/call', (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
